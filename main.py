694a66a273be22c8dffdd60e3afcc70286ea25c8
fedora <nikisavantes@gmail.com>
2025-12-17 20:53:20 +0100

    17/12/2025 draw player

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # main.py
 
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+# from circleshape import CircleShape
+from player import Player
+
 
 def main():
     pygame.init()
@@ -13,14 +16,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y, PLAYER_RADIUS)
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+        # clock.tick(60)
         dt = clock.tick(60) / 1000
         # print(dt)
 
